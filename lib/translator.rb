039efe8d1e7df277b4f56205b2617febe73fd88e
Chribtoper <xcstolfax@gmail.com>
2018-09-16 19:36:14 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,10 +4,10 @@ require "yaml"
 
 def load_library(file)
   emoticons = YAML.load_file('./lib/emoticons.yml')
-  emote_library = {:get_meaning => {}, :get_emoticon => {}}
+  emote_library = {"get_meaning" => {}, "get_emoticon" => {}}
   emoticons.each do |value, emote|
-    emote_library[:get_meaning][emote[1]] = value
-    emote_library[:get_emoticon][emote[0]] = emote[1]
+    emote_library["get_meaning"][emote[1]] = value
+    emote_library["get_emoticon"][emote[0]] = emote[1]
   end
   emote_library
 end
