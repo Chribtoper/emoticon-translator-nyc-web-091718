39e3a87374e598d7ebd1e2efbd5678a3a0be4f49
Chribtoper <xcstolfax@gmail.com>
2018-09-16 19:30:38 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,6 +4,7 @@ require "yaml"
 
 def load_library(file)
   emoticons = YAML.load_file('./lib/emoticons.yml')
+  emote_library = {'get_meaning' => {}, "get_emoticon" => {}}
 end
 
 def get_japanese_emoticon
